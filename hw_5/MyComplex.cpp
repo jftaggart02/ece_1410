d4ef8729eda4b9871fe52c7a9e1e05e68efc4c71
Joshua Taggart <jftaggart02@gmail.com>
2023-09-27 21:07:43 -0600

    v2 of hw_5, satisfied google test

--- hw_5/MyComplex.cpp
+++ hw_5/MyComplex.cpp
@@ -1,5 +1,10 @@
 #include "MyComplex.h"
+#include <sstream>
+#include <iomanip>
 
+using namespace std;
+
+// Constructor with no arguments creates complex number with value 0 + 0i
 MyComplex::MyComplex() {
 
     m_real = 0;
@@ -8,6 +13,7 @@ MyComplex::MyComplex() {
 
 }
 
+// Constructor with arguments initializes complex number with specified values
 MyComplex::MyComplex(float rp, float ip) {
 
     m_real = rp;
@@ -16,22 +22,41 @@ MyComplex::MyComplex(float rp, float ip) {
 
 }
 
+// Returns complex number in string form with 1 digit after decimal point
 string MyComplex::printMyComplex() {
 
     string result;
+    stringstream result_stream;
 
-    result += to_string(m_real);
-    result += " + ";
-    result += to_string(m_imaginary);
-    result += "i";
+    // Set precision of floats to one digit after decimal point
+    result_stream << fixed << setprecision(1);
 
+    // If imaginary part is negative,
+    if (m_imaginary < 0) {
+
+        // Use minus sign between real and imaginary parts
+        result_stream << m_real << " - " << (-1 * m_imaginary) << "i"; 
+
+    }
+    // If imaginary part is positive,
+    else {
+
+        // Use plus sign between real and imaginary parts
+        result_stream << m_real << " + " << m_imaginary << "i"; 
+
+    }
+    
+    result = result_stream.str();
     return result;
 
 }
 
+// Define plus operation on MyComplex objects
 MyComplex MyComplex::operator+(MyComplex right_side) {
 
     MyComplex sum;
+
+    // Add real and imaginary components together 
 
     sum.m_real = this->m_real + right_side.m_real;
 
@@ -41,9 +66,12 @@ MyComplex MyComplex::operator+(MyComplex right_side) {
 
 }
 
+// Define minus operation on MyComplex objects
 MyComplex MyComplex::operator-(MyComplex right_side) {
 
     MyComplex result;
+
+    // Subtract real and imaginary components from each other
 
     result.m_real = this->m_real - right_side.m_real;
 
@@ -53,6 +81,7 @@ MyComplex MyComplex::operator-(MyComplex right_side) {
 
 }
 
+// Define multiply operation on MyComplex objects
 MyComplex MyComplex::operator*(MyComplex right_side) {
 
     MyComplex result;
@@ -62,6 +91,8 @@ MyComplex MyComplex::operator*(MyComplex right_side) {
         c = right_side.m_real,
         d = right_side.m_imaginary;
 
+    // Multiply the complex numbers using this formula
+
     result.m_real = a*c - b*d;
     result.m_imaginary = a*d + b*c;
 
@@ -69,6 +100,7 @@ MyComplex MyComplex::operator*(MyComplex right_side) {
 
 }
 
+// Define divide operation on MyComplex objects
 MyComplex MyComplex::operator/(MyComplex right_side) {
 
     MyComplex result;
@@ -77,6 +109,8 @@ MyComplex MyComplex::operator/(MyComplex right_side) {
         b = this->m_imaginary,
         c = right_side.m_real,
         d = right_side.m_imaginary;
+
+    // Divide the complex numbers using this formula
 
     result.m_real = (a*c + b*d) / (c*c + d*d);
     result.m_imaginary = (b*c - a*d) / (c*c + d*d);
